9502fe9c6ed2a2a5cde532e114b36c26d0ad7066
Mitchell Anton <antonm@student.unimelb.edu.au>
2021-11-15 17:24:28 +1100

    Multiplication and division now working.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   const [output, setOutput] = useState("");
   const [expression, setExpression] = useState("");
   const [clearScreen, setClearScreen] = useState(false);
+  console.log("rendered");
 
   function renderSymbolPressed(e) {
     if (clearScreen) {
@@ -15,15 +16,37 @@ function App() {
     setOutput((output) => output + e.target.textContent);
   }
 
+  function sanitiseExpression() {
+    // We need an expression clone here because we do not want to alter the state of the expression. Otherwise,
+    // this will result in a premature re-render of the application and thus, we will have to press the equals button
+    // twice.
+    let expressionClone = expression.slice();
+    for (let i = 0; i < expression.length; i++) {
+      if (expression.charAt(i) === "x") {
+        expressionClone =  expressionClone.substring(0, i) + "*" + expressionClone.substring(i + 1, expression.length);
+      }
+      else if (expression.charAt(i).charCodeAt() === 247) {
+        expressionClone =  expressionClone.substring(0, i) + "/" + expressionClone.substring(i + 1, expression.length);
+      }
+    }
+
+    return expressionClone;
+  }
+
   function calculateExpression() {
+    if (expression.length === 0) {
+      alert("Please enter an expression.");
+      return;
+    }
     try {
-      setOutput(eval(expression).toString());
+      const expressionClone = sanitiseExpression();
+      setOutput(eval(expressionClone).toString());
       setClearScreen(true);
     } catch (error) {
     }
   }
 
-  // Whenever we update the output, we want to update the expression as well.
+  // Whenever we update the output, we want to update the expression to output as well.
   useEffect(() => {
     setExpression(output);
   }, [output])
